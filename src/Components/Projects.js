27a7b403e43693deb2d3a95cda39952f6fa8b908
MargaretNannyonjo <nannyonjoleon@gmail.com>
2024-08-26 17:02:00 +0300

    changes

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,5 @@
 import React from "react";
 import safari from "../images/safari-screen.png";
-import vista from "../images/vroomvista.png";
 import uh from "../images/uh-page.png";
 import linear from "../images/gradient.png";
 import books from "../images/virtual-care-screen.png";
@@ -12,7 +11,26 @@ export const Projects = () => {
       <h1>My Projects</h1>
       <div className="container">
         <div className="card">
-          <img src={safari} alt="" />
+          <img src={books} alt="Virtualcare" />
+          <p>
+            <span style={{ fontWeight: "bold" }}>Virtual Care:</span>: Designed
+            to facilitate online consultations with healthcare experts, allowing
+            patients to receive medical advice, diagnoses, and treatment from
+            the comfort of their homes.
+          </p>
+
+          <Link
+            to="https://virtalcare-services.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <button>Live Demo </button>
+          </Link>
+        </div>
+
+        <div className="card">
+          <img src={safari} alt="SafariQuest" />
           <p>
             <span style={{ fontWeight: "bold" }}>Safari Quest:</span>{" "}
             Captivating stories, stunning visuals, and delightful recipes take
@@ -31,45 +49,7 @@ export const Projects = () => {
         </div>
 
         <div className="card">
-          <img src={books} alt="" />
-          <p>
-            <span style={{ fontWeight: "bold" }}>Virtual Care:</span>: Designed
-            to facilitate online consultations with healthcare experts, allowing
-            patients to receive medical advice, diagnoses, and treatment from
-            the comfort of their homes.
-          </p>
-
-          <Link
-            to="https://virtual-care-telemedicine.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            <button>Live Demo </button>
-          </Link>
-        </div>
-
-        <div className="card">
-          <img src={vista} alt="" />
-          <p>
-            <span style={{ fontWeight: "bold" }}>VroomVista:</span>
-            Car rental platform for a seamless, hassle-free experience. Explore
-            a diverse fleet, transparent policies, and competitive rates.
-            Effortless vehicle booking for business or leisure.
-          </p>
-
-          <Link
-            to="https://margaretnannyonjo.github.io/car-renting/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            <button> Live Demo </button>
-          </Link>
-        </div>
-
-        <div className="card">
-          <img src={uh} alt="" />
+          <img src={uh} alt="UnityHospital" />
           <p>
             <span style={{ fontWeight: "bold" }}>Unity Hospital:</span>: Your
             gateway to essential medical info, services, and resources. Explore
@@ -88,7 +68,7 @@ export const Projects = () => {
         </div>
 
         <div className="card">
-          <img src={linear} alt="" />
+          <img src={linear} alt="Colorgradient" />
 
           <p>
             <span style={{ fontWeight: "bold" }}>Color Wave:</span> Explore a
